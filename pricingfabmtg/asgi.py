ff95bf7feae614afb2a041718725e963106ec43b
zappabad <gabriel.singhb@gmail.com>
2025-01-06 12:58:38 -0300

    changed project folder structure

--- pricingfabmtg/asgi.py
+++ pricingfabmtg/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pricingfabmtg project.
+ASGI config for myfabpath project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pricingfabmtg.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfabpath.settings')
 
 application = get_asgi_application()
--- pricingfabmtg/wsgi.py
+++ pricingfabmtg/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pricingfabmtg project.
+WSGI config for myfabpath project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pricingfabmtg.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myfabpath.settings')
 
 application = get_wsgi_application()
